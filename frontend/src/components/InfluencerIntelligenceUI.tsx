f5e7d2fa0bd2d7313698ed180fdcb6820986b083
zhenhao23 <weezhenhao@gmail.com>
2025-09-10 05:17:09 +0800

    fix for vercel

--- frontend/src/components/InfluencerIntelligenceUI.tsx
+++ frontend/src/components/InfluencerIntelligenceUI.tsx
@@ -21,8 +21,8 @@ interface InfluencerIntelligenceUIProps {
 
 const InfluencerIntelligenceUI = ({
   data,
-  loading = false,
-}: InfluencerIntelligenceUIProps) => {
+}: // loading = false,
+InfluencerIntelligenceUIProps) => {
   const {
     influencerMetrics,
     influencerInfo,
